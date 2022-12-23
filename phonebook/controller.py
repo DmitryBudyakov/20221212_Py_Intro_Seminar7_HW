a30dd45ba5fee5b10c985e19e91c8b39fc3dc878
Dmitry Budyakov <dnikbu@yandex.ru>
2022-12-23 12:22:02 +0300

    Revision 1

--- phonebook/controller.py
+++ phonebook/controller.py
@@ -4,13 +4,11 @@ import logger
 
 def phonebook_run():
     while True:
-        view.clear_screen()
-        view.app_head()
-        selector = view.phonebook_top_menu()
+        view.cls_show_app_head()
+        selector = view.phonebook_top_menu(top_menu)
         if selector == '1':     # поиск контакта
-            view.clear_screen()
-            view.app_head()
-            view.phonebook_search_menu()
+            view.cls_show_app_head()
+            view.show_sub_menu(sub_menu_search)
             searched_contact = view.search_contact()
             print('Найденные контакты:')
             view.show_contact_head()
@@ -19,29 +17,32 @@ def phonebook_run():
             print()
             input('Нажмите [Enter] чтобы вернуться в Главное меню: ')
         elif selector == '2':   # добавление контакта
-            view.clear_screen()
-            view.app_head()
-            view.phonebook_add_menu()
+            view.cls_show_app_head()
+            view.show_sub_menu(sub_menu_add_contact)
             added_contact = view.add_contact()
             model.write_to_db_file(added_contact, pb_db_file)
             print()
             input('Нажмите [Enter] чтобы вернуться в Главное меню: ')
         elif selector == '3':   # вывод всех контактов
-            view.clear_screen()
-            view.app_head()
-            view.phonebook_all_contacts_menu()
+            view.cls_show_app_head()
+            view.show_sub_menu(sub_menu_show_contacts)
             view.show_contact_head()
             model.print_contacts_with_tabs(model.read_from_file(pb_db_file))
             print()
             input('Нажмите [Enter] чтобы вернуться в Главное меню: ')
         elif selector == '4':   # экспорт контактов
-            view.clear_screen()
-            view.app_head()
-            view.phonebook_export_contacts_menu()
+            view.cls_show_app_head()
+            view.show_sub_menu(sub_menu_export_book)
             all_contacts = model.read_from_file(pb_db_file)
             logger.export_to_txt_file(all_contacts, pb_txt_file)
             print()
             input('Нажмите [Enter] чтобы вернуться в Главное меню: ')
 
 pb_db_file = 'phonebook/phonebook.csv'
-pb_txt_file = 'phonebook/phonebook.txt'    
+pb_txt_file = 'phonebook/phonebook.txt'
+sub_menu_search = '1. Найти контакт\n'
+sub_menu_add_contact = '2. Добавить контакт\n'
+sub_menu_show_contacts = '3. Показать все контакты\n'
+sub_menu_export_book = '4. Экспорт контактов\n'
+menu_item_exit = 'q. Выйти\n'
+top_menu = sub_menu_search + sub_menu_add_contact + sub_menu_show_contacts + sub_menu_export_book + menu_item_exit
--- phonebook/view.py
+++ phonebook/view.py
@@ -9,55 +9,39 @@ def clear_screen():
 
 def app_head():
     """ заголовок программы """
-    title = 'Телефонная книга'
+    title = 'Телефонный аправочник'
     title_line = '-'*len(title)
     head = f'{title_line}\n{title}\n{title_line}'
     print(head)
 
+def cls_show_app_head():
+    clear_screen()
+    app_head()
 
-def phonebook_top_menu() -> str:
+def phonebook_top_menu(menu_top_head: str) -> str:
     """ меню телфонного справочника """
-    menu_top_head =\
-"Главное меню:\n\
-1. Найти контакт\n\
-2. Добавить контакт\n\
-3. Показать все контакты\n\
-4. Экспорт контактов\n\
-q. Выйти\n"
     menu_top_choice = "Выбрать действие [1-4,q]: "
     pb_menu = menu_top_head + menu_top_choice
     actions = ['1','2','3','4','q']
     while True:
         action = input(pb_menu)
         if action not in actions:
-            error_msg = 'Ошибка ввода. Повторите ввод.\n'
-            pb_menu = menu_top_head + error_msg + menu_top_choice
-            clear_screen()
-            app_head()
+            pb_menu = menu_top_head + error_msg_select + menu_top_choice
+            cls_show_app_head()
         elif action == 'q':
             print(msg_on_exit)
             exit()
         else:
             return action
-
-def view_init():
-    clear_screen()
-    app_head()
-    phonebook_top_menu()
-
-def phonebook_search_menu():
-    """ Меню поиска контакта
-    """
-    menu_search_head = \
-"Меню:\n\
-1. Найти контакт\n"
-# \n"
-    clear_screen()
-    app_head()
-    print(menu_search_head)
-
+        
+def show_sub_menu(title: str):
+    """ Показывает подменю """
+    menu_title = f'Меню:\n{title}'
+    cls_show_app_head()
+    print(menu_title)
+    
 def search_contact() -> str:
-    search_string = input('Введите фамилию для поиска: ')
+    search_string = input('Введите данные для поиска: ')
     return search_string
     
 def show_contact_head():
@@ -66,16 +50,6 @@ def show_contact_head():
 "ФАМИЛИЯ\t\tИМЯ\t\tОТЧЕСТВО\t\tТЕЛЕФОН\n\
 -------\t\t---\t\t--------\t\t-------"
     print(contact_head)
-
-def phonebook_add_menu():
-    """ Меню добавления контакта
-    """
-    menu_add_head = \
-"Меню:\n\
-2. Добавить контакт\n"
-    clear_screen()
-    app_head()
-    print(menu_add_head)
 
 def add_contact() -> list:
     """ Добавление контакта
@@ -92,27 +66,7 @@ def add_contact() -> list:
     contact_new.append(phone_number)
     return contact_new
 
-def phonebook_all_contacts_menu():
-    """ Меню показать все контакты
-    """
-    menu_all_cont_head = \
-"Меню:\n\
-3. Показать все контакты\n"
-    clear_screen()
-    app_head()
-    print(menu_all_cont_head)
 
-def phonebook_export_contacts_menu():
-    """ Меню экспорта контактов
-    """
-    menu_export_cont_head = \
-"Меню:\n\
-4. Экспорт контактов\n\
-   (--- в формат txt ---)\n"
-    clear_screen()
-    app_head()
-    print(menu_export_cont_head)
-
-
-global msg_on_exit        
+global msg_on_exit, error_msg       
 msg_on_exit = 'Выход из справочника. Пока!'
+error_msg_select = 'Ошибка ввода. Повторите ввод.\n'
